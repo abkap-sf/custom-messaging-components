77daa5f26508719963e1791d9e9ffed2ce5a3383
abkap-sf <abhinav.kapoor@salesforce.com>
2025-04-30 19:10:20 -0400

    add edits

--- messaging-components/force-app/main/default/lwc/commerceDynamicContentTextRenderer/commerceDynamicContentTextRenderer.js
+++ messaging-components/force-app/main/default/lwc/commerceDynamicContentTextRenderer/commerceDynamicContentTextRenderer.js
@@ -6,51 +6,7 @@ const AGENT = "Agent";
 const CHATBOT = "Chatbot";
 const PARTICIPANT_TYPES = [ENDUSER, AGENT, CHATBOT];
 
-
 export default class CommerceDynamicContentTextRenderer extends LightningElement {
-  mockData = {
-    contentType: "productRecommendations",
-    productHeaderText: "Here are some dressier shoes that go well with your dark jeans",
-    categoryHeaderText: "Would you like to select from styles below",
-    products: [
-      {
-        "name": "Pleated Jacket",
-        "description": "Time to update your assortment with this great suit jacket. You will love its easy fit and classic look.",
-        "price": 139.00,
-        "imageUrl": "https://www.phased-launch-testing.com/on/demandware.static/-/Sites-apparel-m-catalog/default/dwc841dfa6/images/large/PG.10245334.JJ169XX.PZ.jpg"
-      },
-      {
-        "name": "Quilted Jacket",
-        "description": "A classic quilted car coat looks new again. Add a great Commerce Cloud Store top for a perfect look.",
-        "price": 110.99,
-        "imageUrl": "https://www.phased-launch-testing.com/on/demandware.static/-/Sites-apparel-m-catalog/default/dw5d715a5d/images/large/PG.10205921.JJ5FUXX.PZ.jpg"
-      },
-      {
-        "name": "Classic Jacket",
-        "description": "Spring into a new season with this sleek and sophisticated classic two-pocket jacket.",
-        "price": 148.99,
-        "imageUrl": "https://www.phased-launch-testing.com/on/demandware.static/-/Sites-apparel-m-catalog/default/dw683097d4/images/large/PG.10232148.JJC76A6.PZ.jpg"
-      }
-    ],
-    categories: [
-      {
-        "type": "Color",
-        "name": "Black",
-        "id": "JJ169XX"
-      },
-      {
-        "type": "Color",
-        "name": "White",
-        "id": "JJI15XX"
-      },
-      {
-        "type": "Size",
-        "name": "4",
-        "id": "004"
-      }
-    ]
-  }
-
   @api configuration;
 
   _conversationEntry;
@@ -65,56 +21,66 @@ export default class CommerceDynamicContentTextRenderer extends LightningElement
 
   contentType = '';
   productData = [];
+  productDescription = '';
   categoryData = [];
+  categoryDescription = '';
+  productVariants = [];
+  productDetails = {};
   entryPayload = {};
   staticText;
-  productHeaderText = '';
-  categoryHeaderText = '';
   
   processEntryPayload() {
-    this.contentType = 'productRecommendations';
-    this.productData = this.mockData.products;
-    this.categoryData = this.mockData.categories;
-    this.productHeaderText = this.mockData.productHeaderText;
-    this.categoryHeaderText = this.mockData.categoryHeaderText;
+    this.contentType = '';
+    this.productData = [];
+    this.productDescription = '';
+    this.categoryData = [];
+    this.categoryDescription = '';
+    this.productVariants = [];
+    this.productDetails = {};
     this.entryPayload = {};
     this.staticText = undefined;
     this.parsedText = '';
 
-    console.log('product data: ', this.productData);
-    console.log('category data: ', this.categoryData);
+    try {
+      const rawPayload = this._conversationEntry?.entryPayload;
 
-    // try {
-    //   const rawPayload = this._conversationEntry?.entryPayload;
+      try {
+        this.entryPayload = JSON.parse(rawPayload);
+      } catch {
+        this.entryPayload = {
+          abstractMessage: {
+            staticContent: {
+              text: rawPayload
+            }
+          }
+        };
+      }
 
-    //   try {
-    //     this.entryPayload = JSON.parse(rawPayload);
-    //   } catch {
-    //     this.entryPayload = {
-    //       abstractMessage: {
-    //         staticContent: {
-    //           text: rawPayload
-    //         }
-    //       }
-    //     };
-    //   }
+      this.staticText = this.entryPayload?.abstractMessage?.staticContent;
 
-    //   this.staticText = this.entryPayload?.abstractMessage?.staticContent;
+      if (typeof this.staticText?.text === "string" && this.staticText?.text.includes("contentType")) {
+        this.parsedText = JSON.parse(this.staticText.text);
+      } else {
+        this.parsedText = this.staticText?.text;
+      }
 
-    //   if (typeof this.staticText?.text === "string" && this.staticText?.text.includes("contentType")) {
-    //     this.parsedText = JSON.parse(this.staticText.text);
-    //   } else {
-    //     this.parsedText = this.staticText?.text;
-    //   }
+      this.contentType = this.parsedText?.contentType || '';
 
-    //   this.contentType = this.parsedText?.contentType || '';
-
-    //   if (this.isProductRecommendations && this.parsedText?.products) {
-    //     this.productData = this.parsedText.products;
-    //   }
-    // } catch (error) {
-    //   console.error('Failed to process entryPayload:', error);
-    // }
+      if (this.isProductRecommendations && this.parsedText?.productsDetails) {
+        this.productData = this.parsedText.productsDetails.products;
+        this.productDescription = this.parsedText.productsDetails.description;
+        this.categoryData = this.parsedText.categoryDetails.categories;
+        this.categoryDescription = this.parsedText.categoryDetails.description;
+      }
+      if (this.isProductDetails && this.parsedText?.details) {
+        this.productDetails = this.parsedText.details;
+      }
+      if (this.isProductVariants && this.parsedText?.details) {
+        this.productVariants = this.parsedText.details;
+      }
+    } catch (error) {
+      console.error('Failed to process entryPayload:', error);
+    }
   }
 
   get sender() {
@@ -125,11 +91,29 @@ export default class CommerceDynamicContentTextRenderer extends LightningElement
     return this.contentType === 'productRecommendations';
   }
 
+  get isProductVariants() {
+    return this.contentType === 'variant';
+  }
+
+  get isProductDetails() {
+    return this.contentType === 'productDetails';
+  }
+
   handleAddToCart(event) {
     const product = event?.detail?.product?.name;
     if (product) {
       this.configuration.util.sendTextMessage(
         `Can you help me add ${product} with Color Option 'White' and Size Option '6'`
+      );
+    }
+  }
+
+  handleVariantSelection(event) {
+    const { name, option } = event?.detail;
+    console.log('name & option::::', name, option);
+    if (name && option) {
+      this.configuration.util.sendTextMessage(
+        `${name} Option in ${option}`
       );
     }
   }
@@ -157,7 +141,6 @@ export default class CommerceDynamicContentTextRenderer extends LightningElement
   }
 
   get textContent() {
-    console.log('parsed text: ', this.p);
     return this.parsedText;
   }
 }
--- messaging-components/force-app/main/default/lwc/productRecommendationGridMessageComponent/productRecommendationGridMessageComponent.js
+++ messaging-components/force-app/main/default/lwc/productRecommendationGridMessageComponent/productRecommendationGridMessageComponent.js
@@ -1,10 +1,49 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ProductRecommendationGridMessageComponent extends LightningElement {
-    @api products = [];
-    @api productHeaderText = '';
-    @api categoryHeaderText = '';
-    @api categories = [];
+    @api productData = [];
+    @api productDescription = '';
+    @api categoryData = [];
+    @api categoryDescription = '';
+
+    // _productData = {};
+    // _categoryData = {};
+
+    // @api
+    // get productData() {
+    //     return this._productData;
+    // }
+    // set productData(data) {
+    //     console.log('data: ', JSON.parse(data));
+    //     this._productData = data;
+    //     console.log('product data: ', this._productData);
+    // }
+
+    // @api
+    // get categoryData() {
+    //     return this._productData;
+    // }
+    // set categorytData(data) {
+    //     this._categoryData = data;
+    //     console.log('category data: ', this._categoryData);
+    // }
+
+    // get productDescription() {
+    //     return this._productData?.description;
+    // }
+
+    // get categoryDescription() {
+    //     return this._categoryData?.description;
+    // }
+
+    // get products() {
+    //     return this._productData?.products;
+    // }
+
+    // get categories() {
+    //     return this._categoryData?.categories;
+    // }
+
 
     handleAddToCart(event) {
         const index = event.target.dataset.index;
